7901824e16528b18e60ece01d65ad5d2017b794e
Oxtin <austin887@gmail.com>
2020-04-02 15:39:28 +0800

    modify the probability in forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location!');
         } else {
-            callback(undefined, `${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
+			const prob = body.currently.precipProbability * 100;
+            callback(undefined, `${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${prob}% chance of rain.`);
         }
     });
 }
